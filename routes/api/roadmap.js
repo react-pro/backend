ded0b1a8555ab49fc0a7d5ec20d9a34c3c9f714c
Daria Diachuk <dariadar12@gmail.com>
2020-08-26 10:36:03 +0300

    roadmap tmplt

---
+++ routes/api/roadmap.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+
+router.post('/', (req, res, next) => {
+    const results = req.body.results;
+
+});
+
+module.exports = router;
